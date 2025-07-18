cca3b75718b76dd809fd3def409baaa6dbb01ff1
a.espahbodi <dev.amir.espahbodi@gmail.com>
2025-07-18 08:00:52 +0330

    bug fixes

--- preload.py
+++ preload.py
@@ -23,7 +23,7 @@ def main(cfg):
         return
 
     print('Preloading {} dataset...'.format(cfg.dataset.name))
-    combined_resnet_vit = CombinedResNetViT(cfg, "resnet50", cfg.dataset.num_classes)
+    combined_resnet_vit = CombinedResNetViT(cfg, "resnet50", cfg.dataset.num_classes, interpolate_pos_encoding=True)
     
     dataset = generate_dataset(cfg)
     preload_dataset(cfg, dataset, combined_resnet_vit)
