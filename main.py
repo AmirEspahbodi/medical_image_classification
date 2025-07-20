fda2621cbba2f2128f233d7ef0d8c11eb1d292cf
Amir hoseein Espahbodi <101720945+AmirEspahbodi@users.noreply.github.com>
2025-07-20 12:15:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from train import train, evaluate
 from src.utils.metrics import Estimator
 from data.builder import generate_dataset
 from src.builder import generate_model, load_weights
-from src.side_resnet_vit import ‎ResNetSideViTClassifier_MLP‎, ResNetSideViTClassifie_FC, ‎ResNetSideViTClassifier_FFN_MLP‎, ‎ResNetSideViTClassifie_FFN_FC
-
+from src.side_resnet_vit import (
+    ResNetSideViTClassifier_MLP‎, ResNetSideViTClassifie_FC, ‎ResNetSideViTClassifier_FFN_MLP‎, ‎ResNetSideViTClassifie_FFN_FC
+)
 
 
 @hydra.main(config_path="configs", config_name="config")
