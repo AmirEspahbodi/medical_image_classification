f398f30232eacba0497d3b781eca8e92b5f5ee89
Amir hoseein Espahbodi <101720945+AmirEspahbodi@users.noreply.github.com>
2025-07-24 12:02:49 +0200

    Update coatnet_models.py

--- src/coatnet_models.py
+++ src/coatnet_models.py
@@ -317,7 +317,7 @@ class MultiScaleCoAtNetBackbone(nn.Module):
     CNN Backbone using a pre-trained CoAtNet.
     The backbone parameters are FROZEN. It now extracts features from blocks 2, 3, and 4.
     """
-    def __init__(self, model_name, pretrained=True):
+    def __init__(self, model_name, in_chans, pretrained=True):
         super().__init__()
         self.model = timm.create_model(
             model_name,
