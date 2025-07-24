d2e4384402ec3287aa5d182d5aa8044e9289cc54
Amir hoseein Espahbodi <101720945+AmirEspahbodi@users.noreply.github.com>
2025-07-24 11:45:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from train import train, evaluate
 from src.utils.metrics import Estimator
 from data.builder import generate_dataset
 from src.builder import generate_model, load_weights
-from src.coatnet_models import CoAtNetSideViTClassifier_1, CoAtNetSideViTClassifier_2
+from src.coatnet_models import CoAtNetSideViTClassifier_1, CoAtNetSideViTClassifier_2, CoAtNetSideViTClassifier_3
 from src.resnet_models import ResNetSideViTClassifier_1, ResNetSideViTClassifier_2, ResnetSideViTClassifier_3
 
 
