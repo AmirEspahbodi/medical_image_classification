ec2111a49bc5ea7cfc49858aa2adf901bfe575bc
Amir hoseein Espahbodi <101720945+AmirEspahbodi@users.noreply.github.com>
2025-07-26 08:47:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import numpy as np
 from omegaconf import OmegaConf
 
 from src.utils.func import *
-from train import train, evaluate
+from train import train
+from old_train import evaluate
 from src.utils.metrics import Estimator
 from data.builder import generate_dataset
 from src.builder import generate_model, load_weights
